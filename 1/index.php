30cfbb240d05ac234c14cdf22fe5467c7f011481
BatyrovDD <80485048+BatyrovDD@users.noreply.github.com>
2022-04-10 23:53:19 +0500

    Добавил 1 лабу

---
+++ 1/index.php
@@ -0,0 +1,27 @@
+<html>
+<head> <title>PHP Веб-тех. Лаб. работа 1 </title> </head>
+  <a href="http://a0657844.xsph.ru/">Назад</a>
+<div align="center">
+    <h1>PHP Веб-тех. Батыров Денис Дамирович<p> Лаб. работа 1</h1><p>
+</div>
+<body>
+<u1>
+        <li><a href="http://a0657844.xsph.ru/1/lab-1-1.php">Лаба-1-1</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-2.php">Лаба-1-2</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-3.php">Лаба-1-3</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-4.php">Лаба-1-4</a></li>
+        <li><a href="http://a0657844.xsph.ru/1/lab-1-5.php">Лаба-1-5 Варианты 5 и 10</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-6.php">Лаба-1-6</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-7.php">Лаба-1-7</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-8.php">Лаба-1-8</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-9.php">Лаба-1-9</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-10.php">Лаба-1-10</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-11.php">Лаба-1-11</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-12.php">Лаба-1-12</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-13.php">Лаба-1-13</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-14.php">Лаба-1-14</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-15.php">Лаба-2-10</a></li>
+		<li><a href="http://a0657844.xsph.ru/1/lab-1-16.php">Лаба-2-11 (5 и 8 задача)</a></li>
+</u1>
+</body>
+</html>
