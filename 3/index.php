af26429f2f1f62487086ecd3919c1e8d339e246a
BatyrovDD <80485048+BatyrovDD@users.noreply.github.com>
2022-04-11 01:15:18 +0500

    Добавил 3 лабу

---
+++ 3/index.php
@@ -0,0 +1,17 @@
+<html>
+<head> <title>PHP Веб-тех. Лаб. работа 3 </title> </head>
+  <a href="http://a0657844.xsph.ru/">Назад</a>
+<div align="center">
+    <h1>PHP Веб-тех. Батыров Денис Дамирович<p> Лаб. работа 3</h1><p>
+</div>
+<body>
+<u1>
+        <li><a href="http://a0657844.xsph.ru/3/lab-3-1.html">Лаба-3-1</a></li>
+        <li><a href="http://a0657844.xsph.ru/3/lab-3-2.php">Лаба-3-2</a></li>
+		<li><a href="http://a0657844.xsph.ru/3/lab-3-3.php">Лаба-3-3</a></li>
+		<li><a href="http://a0657844.xsph.ru/3/lab-3-4.php">Лаба-3-4</a></li>
+		<li><a href="http://a0657844.xsph.ru/3/lab-3-5.php">Лаба-3-5</a></li>
+		<li><a href="http://a0657844.xsph.ru/3/lab-3-6.html">Лаба-3-6 (5 Вариант)</a></li>
+</u1>
+</body>
+</html>
